2e3fb2179a14a5b783c4997892d7da3d590bcbba
Ashmit Sharma <75840108+Ashmit-cpp@users.noreply.github.com>
2022-04-10 16:04:06 +0530

    Update Home.py

--- Back-end/Home.py
+++ Back-end/Home.py
@@ -1,18 +1,18 @@
 from Module import *
 
-class Home(QWidget):
-    def __init__(self, MainWindow): 
+class HomeC(QWidget):
+    def __init__(self, MainWindow):
         super().__init__(MainWindow)
         self.MainWindow = MainWindow
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        self.verticalLayout_7 = QVBoxLayout()
+        self.setLayout(self.verticalLayout_7)
         self.setContentsMargins(100, 30, 100, 30)
 
         self.logo = QPixmap("../images/eci-logo.png")
         self.logolabel = QLabel()
         self.logolabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.logolabel.setPixmap(self.logo.scaled(500, 120, Qt.AspectRatioMode.KeepAspectRatio,Qt.TransformationMode.SmoothTransformation))
-        self.layout.addWidget(self.logolabel)
+        self.verticalLayout_7.addWidget(self.logolabel)
         
         # election  ->  candidateid, post ,date, vote
         # Table for upcoming election
@@ -25,8 +25,8 @@ class Home(QWidget):
         self.table.setMaximumHeight(300)
         self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
         self.table.setHorizontalHeaderLabels(["Date", "Post", "Assembly"])
-        self.layout.addWidget(self.table)
-        self.layout.addStretch()
+        self.verticalLayout_7.addWidget(self.table)
+        self.verticalLayout_7.addStretch()
 
         self.setStyleSheet("""
             QTableWidget{
