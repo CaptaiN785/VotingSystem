c2e66dd8ac3fb750f20fe6c29d12cc4d913b4826
Ashmit Sharma <75840108+Ashmit-cpp@users.noreply.github.com>
2022-04-21 20:56:45 +0530

    Update ui_new.py

--- Back-end/ui_new.py
+++ Back-end/ui_new.py
@@ -16,7 +16,208 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setSpacing(0)from PyQt6 import QtCore, QtGui, QtWidgets
+from Home import *
+from AddVoter import *
+from AddCandidate import *
+from ElectionDate import *
+from Analytics import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 500)
+        MainWindow.setMinimumSize(QtCore.QSize(1000, 500))
+        MainWindow.setStyleSheet("background-color: rgb(45, 45, 45);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        ######################### TOP BAR ############################
+        self.Top_Bar = QtWidgets.QFrame(self.centralwidget)
+        self.Top_Bar.setMaximumSize(QtCore.QSize(16777215, 40))
+        self.Top_Bar.setStyleSheet("background-color: rgb(35, 35, 35);")
+        self.Top_Bar.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+        self.Top_Bar.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.Top_Bar.setObjectName("Top_Bar")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.Top_Bar)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+
+        ######################### FRAME TOGGLE ############################
+        self.frame_toggle = QtWidgets.QFrame(self.Top_Bar)
+        self.frame_toggle.setMaximumSize(QtCore.QSize(70, 40))
+        self.frame_toggle.setStyleSheet("background-color: rgb(85, 170, 255);")
+        self.frame_toggle.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_toggle.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_toggle.setObjectName("frame_toggle")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame_toggle)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        ######################### TOGGLE BUTTON ############################
+        self.Btn_Toggle = QtWidgets.QPushButton(self.frame_toggle)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
+                                           QtWidgets.QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.Btn_Toggle.sizePolicy().hasHeightForWidth())
+        self.Btn_Toggle.setSizePolicy(sizePolicy)
+        self.Btn_Toggle.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                      "border: 0px solid;"
+                                      "font-size:24px;")
+        self.Btn_Toggle.setText("\u2261")
+        self.Btn_Toggle.setObjectName("Btn_Toggle")
+
+        #################### TOP FRAME ########################################
+        self.verticalLayout_2.addWidget(self.Btn_Toggle)
+        self.horizontalLayout.addWidget(self.frame_toggle)
+        self.frame_top = QtWidgets.QFrame(self.Top_Bar)
+        self.frame_top.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_top.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_top.setObjectName("frame_top")
+        self.horizontalLayout.addWidget(self.frame_top)
+        self.verticalLayout.addWidget(self.Top_Bar)
+
+        ######################## MAIN CONTENT #########################
+        self.Content = QtWidgets.QFrame(self.centralwidget)
+        self.Content.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+        self.Content.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.Content.setObjectName("Content")
+
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.Content)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+
+
+        ##################### TOP MENU FRAME################################
+        self.frame_left_menu = QtWidgets.QFrame(self.Content)
+        self.frame_left_menu.setMinimumSize(QtCore.QSize(70, 0))
+        self.frame_left_menu.setMaximumSize(QtCore.QSize(70, 16777215))
+        self.frame_left_menu.setStyleSheet("background-color: rgb(35, 35, 35);")
+        self.frame_left_menu.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_left_menu.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_left_menu.setObjectName("frame_left_menu")
+
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.frame_left_menu)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+
+
+        ######################### LEFT MENU FRAME ###########################
+        self.frame_top_menus = QtWidgets.QFrame(self.frame_left_menu)
+        self.frame_top_menus.setMinimumSize(QtCore.QSize(70, 120))
+        self.frame_top_menus.setMaximumSize(QtCore.QSize(70, 16777215))
+        self.frame_top_menus.setStyleSheet("background-color: rgb(35, 35, 35);")
+        self.frame_top_menus.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_top_menus.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_top_menus.setObjectName("frame_top_menus")
+
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.frame_top_menus)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+
+
+        ###################### BUTTON FOR HOME############################
+        self.btn_Home = QtWidgets.QPushButton()
+        self.verticalLayout_3.addWidget(self.btn_Home)
+        self.btn_Home.setMinimumSize(QtCore.QSize(70, 40))
+        # self.btn_Home.setText(u"\u2261")
+        self.btn_Home.setIcon(QIcon("../images/homeicon.png"))
+        self.btn_Home.setStyleSheet("QPushButton {\n"
+                                            "    color: rgb(255, 255, 255);\n"
+                                            "    background-color: rgb(35, 35, 35);\n"
+                                            "    border: 0px solid;\n"
+                                            "}\n"
+                                            "QPushButton:hover {\n"
+                                            "    background-color: rgb(85, 170, 255);\n"
+                                            "}")
+        self.btn_Home.setObjectName("btn_Home")
+        self.verticalLayout_4.addWidget(self.btn_Home)
+
+
+        ###################### BUTTON TO ADD VOTER ###################
+        self.btn_AddVoter = QtWidgets.QPushButton()
+
+        self.verticalLayout_3.addWidget(self.btn_AddVoter)
+        self.btn_AddVoter.setIcon(QIcon("../images/addmember.png"))
+        self.btn_AddVoter.setMinimumSize(QtCore.QSize(70, 40))
+        #sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Fixed)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        self.btn_AddVoter.setStyleSheet("QPushButton {\n"
+                                            "    color: rgb(255, 255, 255);\n"
+                                            "    background-color: rgb(35, 35, 35);\n"
+                                            "    border: 0px solid;\n"
+                                            "}\n"
+                                            "QPushButton:hover {\n"
+                                            "    background-color: rgb(85, 170, 255);\n"
+                                            "}")
+        self.btn_AddVoter.setObjectName("btn_AddVoter")
+        self.verticalLayout_4.addWidget(self.btn_AddVoter)
+
+
+        ###################### BUTTON TO ADD CANDIDATE ###################
+        self.btn_AddCandidate = QtWidgets.QPushButton(self.frame_top_menus)
+        self.btn_AddCandidate.setMinimumSize(QtCore.QSize(70, 40))
+        self.btn_AddCandidate.setStyleSheet("QPushButton {\n"
+                                            "    color: rgb(255, 255, 255);\n"
+                                            "    background-color: rgb(35, 35, 35);\n"
+                                            "    border: 0px solid;\n"
+                                            "}\n"
+                                            "QPushButton:hover {\n"
+                                            "    background-color: rgb(85, 170, 255);\n"
+                                            "}")
+        self.btn_AddCandidate.setObjectName("btn_AddCandidate")
+        self.verticalLayout_4.addWidget(self.btn_AddCandidate)
+        self.btn_AddCandidate.setIcon(QIcon("../images/plusicon.png"))
+
+
+
+        ########################### ADD FRAMES ###########################
+        self.horizontalLayout_2.addWidget(self.frame_left_menu)
+        self.frame_pages = QtWidgets.QFrame(self.Content)
+        self.frame_pages.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame_pages.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame_pages.setObjectName("frame_pages")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.frame_pages)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.stackedWidget = QtWidgets.QStackedWidget(self.frame_pages)
+        self.stackedWidget.setObjectName("stackedWidget")
+
+
+        ############## ADD BUTTONS TO STACKED WIDGET #####################
+        self.Home = HomeC(MainWindow)
+        self.Home.setObjectName("Home")
+        self.stackedWidget.addWidget(self.Home)
+
+        self.AddVoter = AddVoterC(MainWindow)
+        self.AddVoter.setObjectName("Home")
+        self.stackedWidget.addWidget(self.AddVoter)
+
+        self.AddCandidate = AddCandidateC(MainWindow)
+        self.AddCandidate.setObjectName("Home")
+        self.stackedWidget.addWidget(self.AddCandidate)
+
+
+        ########################### ADD STACKED WIDGET ####################
+        self.verticalLayout_5.addWidget(self.stackedWidget)
+        self.horizontalLayout_2.addWidget(self.frame_pages)
+        self.verticalLayout.addWidget(self.Content)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.stackedWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
         self.verticalLayout.setObjectName("verticalLayout")
 
         ######################### TOP BAR ############################
