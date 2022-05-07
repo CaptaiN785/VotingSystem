1a4e1bed9289cadd798d7ef062ba02489719b36d
Mukesh Kumar Thakur <thakurmukeshkumar785@gmail.com>
2022-05-07 20:19:35 +0530

    Switched to php from node js

--- Frontend/server.js
+++ Frontend/server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var connection = mysql.createConnection({
     host: 'localhost',//host name
     user: 'root',// username of database
-    password: '12345',//Password of database
+    password: 'Mukesh@2001',//Password of database
     database : 'votingsystem'//Database name
 });
 
@@ -21,6 +21,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
+// app.set("view engine", "pug");  // install pug using this command
+
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '/login.html'));
 });
@@ -29,18 +31,29 @@ app.post('/auth', function(request, response) {
     var username = request.body.username;
     console.log(username)
     if (username) {
+        var AID = -1;
         connection.query('SELECT * FROM voter WHERE VOTERID = ?', [username], function(error, results, fields) {
             if (results.length > 0) {
                 request.session.loggedin = true;
                 request.session.username = username;
-                response.redirect(path.join('/home.html'));
-            } else {
-                                response.send("<script language=javascript>alert('Invalid Voter ID'); history.back();</script>");
-            }    
+                AID = results[0].AID;
 
+                // var query = "select * from election where Date >= current_date() and AID = ?";
+                // var Election_data = null;
+                // connection.query(query,[AID], function(error, election_data, fields){
+                //     if(error) throw err;
+                //     Election_data = election_data;
+                // });
+                // response.render(path.join(__dirname, "home"));
+                // response.redirect('home.html')
+                response.render(path.join(__dirname, 'home.html'))
+                console.log("home page should appear.");
+            }else {
+                response.send("<script language=javascript>alert('Invalid Voter ID'); history.back();</script>");
+            }
             response.end();
         });
-    } 
+    }
 });
 
 app.listen(5000);
