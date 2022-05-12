53f248204684b343ed6fd31cff1ff9e61cb9ae88
Mukesh Kumar Thakur <thakurmukeshkumar785@gmail.com>
2022-05-12 14:33:49 +0530

    Updated footer

--- Frontend/include/footer.php
+++ Frontend/include/footer.php
@@ -7,5 +7,10 @@
 <script src="js/jquery.easing.min.js"></script>
 <script src="js/sticky-menu.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <div class = "lead" style ="background:pink; text-align:center;padding:10px;">
+        <p>Made with <span style = "color:#f00;">&#10084;</span> by Mukesh, Ashmit and Sayan</p>
+    </div>
+
 </body>
 </html>
